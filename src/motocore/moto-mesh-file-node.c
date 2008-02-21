2232c229f2121975f99da377ad3ea43ac03575b4
Konstantin A. Evdokimenko a.k.a Qew[erty] <qewerty@gmail.com>
2008-02-22 04:34:41 +0700

    Fix some bugs in MotoMeshFileNode class

--- src/motocore/moto-mesh-file-node.c
+++ src/motocore/moto-mesh-file-node.c
@@ -2,12 +2,20 @@
 #include "moto-mesh-param-data.h"
 #include "moto-filename-param-data.h"
 
+/* forwards */
+
+static gchar *moto_mesh_file_node_get_filename(MotoMeshFileNode *self);
+static void moto_mesh_file_node_set_filename(MotoMeshFileNode *self, const gchar *filename);
+
+static MotoMesh *moto_mesh_file_node_get_mesh(MotoMeshFileNode *self);
+
 /* class MeshFileNode */
 
 static GObjectClass *mesh_file_node_parent_class = NULL;
 
 struct _MotoMeshFileNodePriv
 {
+    GString *filename;
     MotoMesh *mesh;
 };
 
@@ -16,6 +24,8 @@ moto_mesh_file_node_dispose(GObject *obj)
 {
     MotoMeshFileNode *self = (MotoMeshFileNode *)obj;
 
+    g_string_free(self->priv->filename, TRUE);
+    moto_mesh_unref(self->priv->mesh);
     g_slice_free(MotoMeshFileNodePriv, self->priv);
 
     mesh_file_node_parent_class->dispose(obj);
@@ -45,9 +55,26 @@ G_DEFINE_TYPE(MotoMeshFileNode, moto_mesh_file_node, MOTO_TYPE_GEOMETRY_NODE);
 
 /* methods of class MeshFileNode */
 
+static gpointer get_filename(MotoParam *param)
+{
+    return moto_mesh_file_node_get_filename((MotoMeshFileNode *)moto_param_get_node(param));
+}
+
+static void set_filename(MotoParam *param, gpointer p)
+{
+    moto_mesh_file_node_set_filename((MotoMeshFileNode *)moto_param_get_node(param),
+            (gchar *)p);
+}
+
+static gpointer get_mesh(MotoParam *param)
+{
+    return moto_mesh_file_node_get_mesh((MotoMeshFileNode *)moto_param_get_node(param));
+}
+
 MotoMeshFileNode *moto_mesh_file_node_new()
 {
     MotoMeshFileNode *self = (MotoMeshFileNode *)g_object_new(MOTO_TYPE_MESH_FILE_NODE, NULL);
+    MotoNode *node = (MotoNode *)self;
 
     /* params */
 
@@ -63,4 +90,19 @@ MotoMeshFileNode *moto_mesh_file_node_new()
             moto_mesh_param_data_new(get_mesh, NULL, NULL));
 
     return self;
+}
+
+static gchar *moto_mesh_file_node_get_filename(MotoMeshFileNode *self)
+{
+    return self->priv->filename->str;
+}
+
+static void moto_mesh_file_node_set_filename(MotoMeshFileNode *self, const gchar *filename)
+{
+    g_string_assign(self->priv->filename, filename);
+}
+
+static MotoMesh *moto_mesh_file_node_get_mesh(MotoMeshFileNode *self)
+{
+    return self->priv->mesh;
 }
