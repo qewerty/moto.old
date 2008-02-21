f268aa665ca66d0af1e72115f2cc2634f1c6b0f5
Konstantin A. Evdokimenko a.k.a Qew[erty] <qewerty@gmail.com>
2008-02-21 10:53:06 +0700

    Added classes MotoTransformStrategyParamData, MotoTransformOrderParamData and MotoRotateOrderParamData

---
+++ src/motocore/moto-rotate-order-param-data.c
@@ -0,0 +1,52 @@
+#include "moto-rotate-order-param-data.h"
+
+/* class RotateOrderParamData */
+
+static GObjectClass *rotate_order_param_data_parent_class = NULL;
+
+static void
+moto_rotate_order_param_data_dispose(GObject *obj)
+{
+    G_OBJECT_CLASS(rotate_order_param_data_parent_class)->dispose(obj);
+}
+
+static void
+moto_rotate_order_param_data_finalize(GObject *obj)
+{
+    rotate_order_param_data_parent_class->finalize(obj);
+}
+
+static void
+moto_rotate_order_param_data_init(MotoRotateOrderParamData *self)
+{
+    self->value = self->default_value = FALSE;
+}
+
+static void
+moto_rotate_order_param_data_class_init(MotoRotateOrderParamDataClass *klass)
+{
+    rotate_order_param_data_parent_class = (GObjectClass *)g_type_class_peek_parent(klass);
+
+    rotate_order_param_data_parent_class->dispose = moto_rotate_order_param_data_dispose;
+    rotate_order_param_data_parent_class->finalize = moto_rotate_order_param_data_finalize;
+}
+
+G_DEFINE_TYPE(MotoRotateOrderParamData, moto_rotate_order_param_data, MOTO_TYPE_PARAM_DATA);
+
+/* methods of class RotateOrderParamData */
+
+MotoParamData *
+moto_rotate_order_param_data_new(MotoParamDataGetFunc get,
+        MotoParamDataSetFunc set, MotoRotateOrder default_value)
+{
+    MotoRotateOrderParamData *self = \
+        (MotoRotateOrderParamData *)g_object_new(MOTO_TYPE_ROTATE_ORDER_PARAM_DATA, NULL);
+    MotoParamData *data = (MotoParamData *)self;
+
+    data->get = get;
+    data->set = set;
+
+    self->value = self->default_value = default_value;
+
+    return data;
+}
---
+++ src/motocore/moto-transform-order-param-data.c
@@ -0,0 +1,52 @@
+#include "moto-transform-order-param-data.h"
+
+/* class TransformOrderParamData */
+
+static GObjectClass *transform_order_param_data_parent_class = NULL;
+
+static void
+moto_transform_order_param_data_dispose(GObject *obj)
+{
+    G_OBJECT_CLASS(transform_order_param_data_parent_class)->dispose(obj);
+}
+
+static void
+moto_transform_order_param_data_finalize(GObject *obj)
+{
+    transform_order_param_data_parent_class->finalize(obj);
+}
+
+static void
+moto_transform_order_param_data_init(MotoTransformOrderParamData *self)
+{
+    self->value = self->default_value = FALSE;
+}
+
+static void
+moto_transform_order_param_data_class_init(MotoTransformOrderParamDataClass *klass)
+{
+    transform_order_param_data_parent_class = (GObjectClass *)g_type_class_peek_parent(klass);
+
+    transform_order_param_data_parent_class->dispose = moto_transform_order_param_data_dispose;
+    transform_order_param_data_parent_class->finalize = moto_transform_order_param_data_finalize;
+}
+
+G_DEFINE_TYPE(MotoTransformOrderParamData, moto_transform_order_param_data, MOTO_TYPE_PARAM_DATA);
+
+/* methods of class TransformOrderParamData */
+
+MotoParamData *
+moto_transform_order_param_data_new(MotoParamDataGetFunc get,
+        MotoParamDataSetFunc set, MotoTransformOrder default_value)
+{
+    MotoTransformOrderParamData *self = \
+        (MotoTransformOrderParamData *)g_object_new(MOTO_TYPE_TRANSFORM_ORDER_PARAM_DATA, NULL);
+    MotoParamData *data = (MotoParamData *)self;
+
+    data->get = get;
+    data->set = set;
+
+    self->value = self->default_value = default_value;
+
+    return data;
+}
