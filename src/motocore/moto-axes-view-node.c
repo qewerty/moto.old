7bb3e58541687dc3830ddbe914120b3df7dea271
Konstantin A. Evdokimenko a.k.a Qew[erty] <qewerty@gmail.com>
2008-03-08 06:03:46 +0700

    Added files moto-axes-view-node.h and --//--.c

---
+++ src/motocore/moto-axes-view-node.c
@@ -0,0 +1,216 @@
+#include "math.h"
+
+#include "GL/gl.h"
+#include "GL/glu.h"
+
+#include "moto-axes-view-node.h"
+#include "moto-geometry-view-param-data.h"
+
+/* forwards */
+
+static void moto_axes_view_node_draw(MotoGeometryViewNode *self);
+static void moto_axes_view_node_prepare_for_draw(MotoGeometryViewNode *self);
+
+/* class AxesViewNode */
+
+static GObjectClass *axes_view_node_parent_class = NULL;
+
+struct _MotoAxesViewNodePriv
+{
+    gboolean prepared;
+    GLuint dlist;
+};
+
+static void
+moto_axes_view_node_dispose(GObject *obj)
+{
+    MotoAxesViewNode *self = (MotoAxesViewNode *)obj;
+
+    g_slice_free(MotoAxesViewNodePriv, self->priv);
+
+    G_OBJECT_CLASS(axes_view_node_parent_class)->dispose(obj);
+}
+
+static void
+moto_axes_view_node_finalize(GObject *obj)
+{
+    axes_view_node_parent_class->finalize(obj);
+}
+
+static void
+moto_axes_view_node_init(MotoAxesViewNode *self)
+{
+    self->priv = g_slice_new(MotoAxesViewNodePriv);
+
+    self->priv->prepared = FALSE;
+    self->priv->dlist = 0;
+}
+
+static void
+moto_axes_view_node_class_init(MotoAxesViewNodeClass *klass)
+{
+    GObjectClass *goclass = G_OBJECT_CLASS(klass);
+    MotoGeometryViewNodeClass *gvnclass = \
+        MOTO_GEOMETRY_VIEW_NODE_CLASS(klass);
+
+    axes_view_node_parent_class = (GObjectClass *)g_type_class_peek_parent(klass);
+
+    goclass->dispose    = moto_axes_view_node_dispose;
+    goclass->finalize   = moto_axes_view_node_finalize;
+
+    gvnclass->draw              = moto_axes_view_node_draw;
+    gvnclass->prepare_for_draw  = moto_axes_view_node_prepare_for_draw;
+}
+
+G_DEFINE_TYPE(MotoAxesViewNode, moto_axes_view_node, MOTO_TYPE_GEOMETRY_VIEW_NODE);
+
+/* methods of class AxesViewNode */
+
+static gpointer get_view(MotoParam *param)
+{
+    return moto_param_get_node(param);
+}
+
+MotoAxesViewNode *moto_axes_view_node_new(const gchar *name)
+{
+    MotoAxesViewNode *self = (MotoAxesViewNode *)g_object_new(MOTO_TYPE_AXES_VIEW_NODE, NULL);
+    MotoNode *node = (MotoNode *)self;
+
+    moto_node_set_name(node, name);
+
+    MotoParamBlock *pb;
+    MotoParamData *pdata;
+
+    /* params */
+
+    pb = moto_param_block_new("main", "Main", (MotoNode *)self);
+    moto_node_add_param_block(node, pb);
+
+    moto_param_new("view", "Geometry View", MOTO_PARAM_MODE_OUT, pb,
+            pdata = moto_geometry_view_param_data_new(NULL));
+    moto_param_data_set_cbs(pdata, NULL, NULL, get_view, NULL);
+
+    return self;
+}
+
+static void draw_axes()
+{
+    glLineWidth(2);
+
+    glColor4f(1, 0, 0, 1);
+    glBegin(GL_LINES);
+    glVertex3f(0, 0, 0);
+    glVertex3f(2, 0, 0);
+    glEnd();
+
+    glColor4f(0, 1, 0, 1);
+    glBegin(GL_LINES);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 2, 0);
+    glEnd();
+
+    glColor4f(0, 0, 1, 1);
+    glBegin(GL_LINES);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 0, 2);
+    glEnd();
+}
+
+static void moto_axes_view_node_draw(MotoGeometryViewNode *self)
+{
+    MotoAxesViewNode *view = (MotoAxesViewNode *)self;
+
+    // glPushAttrib();
+
+    // glUseProgramObjectARB(0);
+
+    if( ! view->priv->prepared)
+        moto_axes_view_node_prepare_for_draw(self);
+    else
+        glCallList(view->priv->dlist);
+
+    // glPopAttrib();
+}
+
+static void moto_axes_view_node_prepare_for_draw(MotoGeometryViewNode *self)
+{
+    MotoAxesViewNode *view = (MotoAxesViewNode *)self;
+
+    if( ! glIsList(view->priv->dlist))
+        view->priv->dlist = glGenLists(1);
+
+    glNewList(view->priv->dlist, GL_COMPILE_AND_EXECUTE);
+
+    draw_axes();
+
+    glEndList();
+
+    view->priv->prepared = TRUE;
+}
+
+/* class AxesViewNodeFactory */
+
+GType moto_axes_view_node_factory_get_node_type(MotoNodeFactory *self);
+
+MotoNode *
+moto_axes_view_node_factory_create_node(MotoNodeFactory *self,
+        const gchar *name);
+
+static GObjectClass *axes_view_node_factory_parent_class = NULL;
+
+static void
+moto_axes_view_node_factory_dispose(GObject *obj)
+{
+    G_OBJECT_CLASS(axes_view_node_factory_parent_class)->dispose(obj);
+}
+
+static void
+moto_axes_view_node_factory_finalize(GObject *obj)
+{
+    axes_view_node_factory_parent_class->finalize(obj);
+}
+
+static void
+moto_axes_view_node_factory_init(MotoAxesViewNodeFactory *self)
+{}
+
+static void
+moto_axes_view_node_factory_class_init(MotoAxesViewNodeFactoryClass *klass)
+{
+    GObjectClass *goclass = (GObjectClass *)klass;
+    MotoNodeFactoryClass *nfclass = (MotoNodeFactoryClass *)klass;
+
+    axes_view_node_factory_parent_class = (GObjectClass *)g_type_class_peek_parent(klass);
+
+    goclass->dispose    = moto_axes_view_node_factory_dispose;
+    goclass->finalize   = moto_axes_view_node_factory_finalize;
+
+    nfclass->get_node_type  = moto_axes_view_node_factory_get_node_type;
+    nfclass->create_node    = moto_axes_view_node_factory_create_node;
+}
+
+G_DEFINE_TYPE(MotoAxesViewNodeFactory, moto_axes_view_node_factory, MOTO_TYPE_NODE_FACTORY);
+
+/* methods of class AxesViewNodeFactory */
+
+static MotoNodeFactory *axes_view_node_factory = NULL;
+
+MotoNodeFactory *moto_axes_view_node_factory_new()
+{
+    if( ! axes_view_node_factory)
+        axes_view_node_factory = \
+            (MotoNodeFactory *)g_object_new(MOTO_TYPE_AXES_VIEW_NODE_FACTORY, NULL);
+
+    return axes_view_node_factory;
+}
+
+GType moto_axes_view_node_factory_get_node_type(MotoNodeFactory *self)
+{
+    return MOTO_TYPE_AXES_VIEW_NODE;
+}
+
+MotoNode *moto_axes_view_node_factory_create_node(MotoNodeFactory *self,
+        const gchar *name)
+{
+    return (MotoNode *)moto_axes_view_node_new(name);
+}
---
+++ src/motocore/moto-axes-view-node.h
@@ -0,0 +1,83 @@
+/* ##################################################################################
+#
+#  Moto Animation System (http://motoanim.sf.net)
+#  Copyleft (C) 2008 Konstantin Evdokimenko a.k.a Qew[erty] (qewerty@gmail.com)
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+################################################################################## */
+
+#ifndef MOTO_AXES_VIEW_NODE_H
+#define MOTO_AXES_VIEW_NODE_H
+
+#include "moto-geometry-view-node.h"
+
+typedef struct _MotoAxesViewNode MotoAxesViewNode;
+typedef struct _MotoAxesViewNodeClass MotoAxesViewNodeClass;
+typedef struct _MotoAxesViewNodePriv MotoAxesViewNodePriv;
+
+typedef struct _MotoAxesViewNodeFactory MotoAxesViewNodeFactory;
+typedef struct _MotoAxesViewNodeFactoryClass MotoAxesViewNodeFactoryClass;
+typedef struct _MotoAxesViewNodeFactoryPriv MotoAxesViewNodeFactoryPriv;
+
+/* class MotoAxesViewNode */
+
+struct _MotoAxesViewNode
+{
+    MotoGeometryViewNode parent;
+
+    MotoAxesViewNodePriv *priv;
+};
+
+struct _MotoAxesViewNodeClass
+{
+    MotoGeometryViewNodeClass parent;
+};
+
+GType moto_axes_view_node_storage_get_type(void);
+
+#define MOTO_TYPE_AXES_VIEW_NODE (moto_axes_view_node_get_type())
+#define MOTO_AXES_VIEW_NODE(obj)  (G_TYPE_CHECK_INSTANCE_CAST ((obj), MOTO_TYPE_AXES_VIEW_NODE, MotoAxesViewNode))
+#define MOTO_AXES_VIEW_NODE_CLASS(klass)  (G_TYPE_CHECK_CLASS_CAST ((klass), MOTO_TYPE_AXES_VIEW_NODE, MotoAxesViewNodeClass))
+#define MOTO_IS_AXES_VIEW_NODE(obj)  (G_TYPE_CHECK_INSTANCE_TYPE ((obj),MOTO_TYPE_AXES_VIEW_NODE))
+#define MOTO_IS_AXES_VIEW_NODE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),MOTO_TYPE_AXES_VIEW_NODE))
+#define MOTO_AXES_VIEW_NODE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),MOTO_TYPE_AXES_VIEW_NODE, MotoAxesViewNodeClass))
+
+MotoAxesViewNode *moto_axes_view_node_new(const gchar *name);
+
+/* class MotoAxesViewNodeFactory */
+
+struct _MotoAxesViewNodeFactory
+{
+    MotoNodeFactory parent;
+};
+
+struct _MotoAxesViewNodeFactoryClass
+{
+    MotoNodeFactoryClass parent;
+};
+
+GType moto_axes_view_node_factory_get_type(void);
+
+#define MOTO_TYPE_AXES_VIEW_NODE_FACTORY (moto_axes_view_node_factory_get_type())
+#define MOTO_AXES_VIEW_NODE_FACTORY(obj)  (G_TYPE_CHECK_INSTANCE_CAST ((obj), MOTO_TYPE_AXES_VIEW_NODE_FACTORY, MotoAxesViewNodeFactory))
+#define MOTO_AXES_VIEW_NODE_FACTORY_CLASS(klass)  (G_TYPE_CHECK_CLASS_CAST ((klass), MOTO_TYPE_AXES_VIEW_NODE_FACTORY, MotoAxesViewNodeFactoryClass))
+#define MOTO_IS_AXES_VIEW_NODE_FACTORY(obj)  (G_TYPE_CHECK_INSTANCE_TYPE ((obj),MOTO_TYPE_AXES_VIEW_NODE_FACTORY))
+#define MOTO_IS_AXES_VIEW_NODE_FACTORY_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),MOTO_TYPE_AXES_VIEW_NODE_FACTORY))
+#define MOTO_AXES_VIEW_NODE_FACTORY_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),MOTO_TYPE_AXES_VIEW_NODE_FACTORY, MotoAxesViewNodeFactoryClass))
+
+MotoNodeFactory *moto_axes_view_node_factory_new();
+
+#endif /* MOTO_AXES_VIEW_NODE_H */
