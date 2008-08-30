00a8a0a6a9ad04b571794b85b296ea1c696f5109
Konstantin A. Evdokimenko a.k.a Qew[erty] <qewerty@gmail.com>
2008-08-31 04:50:01 +0700

    Added marshallers.

---
+++ src/motocore/moto-marshal.h
@@ -0,0 +1,20 @@
+
+#ifndef __moto_cclosure_marshal_MARSHAL_H__
+#define __moto_cclosure_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:OBJECT,INT,INT,INT,INT (marshal.txt:1) */
+extern void moto_cclosure_marshal_VOID__OBJECT_INT_INT_INT_INT (GClosure     *closure,
+                                                                GValue       *return_value,
+                                                                guint         n_param_values,
+                                                                const GValue *param_values,
+                                                                gpointer      invocation_hint,
+                                                                gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* __moto_cclosure_marshal_MARSHAL_H__ */
+
