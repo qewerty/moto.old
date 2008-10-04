aa1651d5bbb604dac37998d7a53d51048be0cd42
Konstantin A. Evdokimenko a.k.a Qew[erty] <qewerty@gmail.com>
2008-10-04 15:54:28 +0700

    Added build configs

---
+++ config/linux2-debug.py
@@ -0,0 +1,6 @@
+MOTO_GUI = 'moto-gui'
+
+CFLAGS = ['-O0', '-g', '-Wall']
+LIBS    = ['GL', 'GLU', 'GLEW']
+
+PKG_CONFIG = 'pkg-config gtk+-2.0 gtkglext-1.0 gio-2.0 gthread-2.0 --cflags --libs'
---
+++ config/linux2-release.py
@@ -0,0 +1,7 @@
+MOTO_GUI = 'moto-gui'
+
+CFLAGS  = ['-O3', '-pipe', '-ffast-math', '-Wall']
+LIBS    = ['GL', 'GLU', 'GLEW']
+
+PKG_CONFIG = 'pkg-config gtk+-2.0 gtkglext-1.0 gio-2.0 gthread-2.0 --cflags --libs'
+
