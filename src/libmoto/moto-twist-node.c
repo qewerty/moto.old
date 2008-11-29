5b5aa64df5d8cab1bd5d1647bdb0574c601fceb8
Konstantin A. Evdokimenko a.k.a Qew[erty] <qewerty@gmail.com>
2008-11-30 00:36:12 +0600

    Files moto-geometry-node.* have been move to moto-geom-node.*

---
+++ src/libmoto/moto-twist-node.c
@@ -0,0 +1,185 @@
+#include <math.h>
+
+
+#include "moto-copyable.h"
+#include "moto-point-cloud.h"
+#include "moto-mesh.h" // FIXME: Temporary!
+#include "moto-displace-node.h"
+#include "libmotoutil/moto-gl.h"
+#include "libmotoutil/matrix.h"
+#include "libmotoutil/numdef.h"
+
+/* forwards */
+
+static void moto_displace_node_update(MotoNode *self);
+
+/* class NormalMoveNode */
+
+typedef struct _MotoDisplaceNodePriv MotoDisplaceNodePriv;
+#define MOTO_DISPLACE_NODE_GET_PRIVATE(obj) G_TYPE_INSTANCE_GET_PRIVATE(obj, MOTO_TYPE_DISPLACE_NODE, MotoDisplaceNodePriv)
+
+static GObjectClass *normal_move_node_parent_class = NULL;
+
+struct _MotoDisplaceNodePriv
+{
+    MotoPointCloud *pc;
+    MotoPointCloud *prev_pc;
+    gfloat prev_scale;
+};
+
+static void
+moto_displace_node_dispose(GObject *obj)
+{
+    normal_move_node_parent_class->dispose(obj);
+}
+
+static void
+moto_displace_node_finalize(GObject *obj)
+{
+    MotoDisplaceNodePriv *priv = MOTO_DISPLACE_NODE_GET_PRIVATE(obj);
+
+    if(priv->pc);
+        g_object_unref(priv->pc);
+
+    normal_move_node_parent_class->finalize(obj);
+}
+
+static void
+moto_displace_node_init(MotoDisplaceNode *self)
+{
+    MotoNode *node = (MotoNode *)self;
+    MotoDisplaceNodePriv *priv = MOTO_DISPLACE_NODE_GET_PRIVATE(self);
+
+    priv->pc = NULL;
+    priv->prev_pc = NULL;
+    priv->prev_scale = 0.0f;
+
+    /* params */
+
+    GParamSpec *pspec = NULL; // FIXME: Implement.
+    moto_node_add_params(node,
+            "in_pc",  "Input Point Cloud",  MOTO_TYPE_POINT_CLOUD, MOTO_PARAM_MODE_IN, NULL, pspec, "Geometry", "Geometry",
+            "out_pc", "Output Point Cloud", MOTO_TYPE_POINT_CLOUD, MOTO_PARAM_MODE_OUT, NULL, pspec, "Geometry", "Geometry",
+            "scale",  "Scaling Value",        G_TYPE_FLOAT, MOTO_PARAM_MODE_INOUT, 0.1f, pspec, "Geometry", "Geometry",
+            NULL);
+}
+
+static void
+moto_displace_node_class_init(MotoDisplaceNodeClass *klass)
+{
+    g_type_class_add_private(klass, sizeof(MotoDisplaceNodePriv));
+
+    normal_move_node_parent_class = (GObjectClass *)g_type_class_peek_parent(klass);
+
+    GObjectClass *goclass = G_OBJECT_CLASS(klass);
+    MotoNodeClass *nclass = (MotoNodeClass *)klass;
+
+    goclass->dispose    = moto_displace_node_dispose;
+    goclass->finalize   = moto_displace_node_finalize;
+
+    nclass->update = moto_displace_node_update;
+}
+
+G_DEFINE_TYPE(MotoDisplaceNode, moto_displace_node, MOTO_TYPE_NODE);
+
+/* methods of class NormalMoveNode */
+
+MotoDisplaceNode *moto_displace_node_new(const gchar *name)
+{
+    MotoDisplaceNode *self = (MotoDisplaceNode *)g_object_new(MOTO_TYPE_DISPLACE_NODE, NULL);
+    MotoNode *node = (MotoNode *)self;
+
+    moto_node_set_name(node, name);
+
+    return self;
+}
+
+static void moto_displace_node_update(MotoNode *self)
+{
+    MotoDisplaceNodePriv *priv = MOTO_DISPLACE_NODE_GET_PRIVATE(self);
+
+    MotoPointCloud *in_pc = (MotoPointCloud *)moto_node_get_param_object(self, "in_pc");
+    if( ! in_pc)
+    {
+        if(priv->pc)
+        {
+            g_object_unref(priv->pc);
+        }
+        priv->pc = NULL;
+        return;
+    }
+    else
+    {
+        if(in_pc != priv->prev_pc)
+        {
+            if(priv->pc)
+            {
+                g_object_unref(priv->pc);
+            }
+            priv->pc = MOTO_POINT_CLOUD(moto_copyable_copy(MOTO_COPYABLE(in_pc)));
+            moto_mesh_prepare(MOTO_MESH(priv->pc));
+        }
+        MotoPointCloud *pc = priv->pc;
+        priv->prev_pc = in_pc;
+
+        gfloat scale = moto_node_get_param_float(self, "scale");
+        if(fabs(scale-priv->prev_scale) <= MICRO)
+            return;
+        priv->prev_scale = scale;
+
+        if(moto_point_cloud_can_provide_plain_data(pc))
+        {
+            gfloat *points_i  = NULL;
+            gfloat *normals_i = NULL;
+            gsize size_i      = 0;
+            gfloat *points_o  = NULL;
+            gfloat *normals_o = NULL;
+            gsize size_o      = 0;
+
+            moto_point_cloud_get_plain_data(in_pc, & points_i, & normals_i, & size_i);
+            moto_point_cloud_get_plain_data(pc,    & points_o, & normals_o, & size_o);
+
+            gfloat axis[3] = {0, 1, 0};
+            gfloat m[16];
+
+            gint i;
+            #pragma omp parallel for private(i)
+            for(i = 0; i < size_i; i++)
+            {
+                gfloat *pi = points_i + i*3;
+                // gfloat *ni = normals_i + i*3;
+                gfloat *po = points_o + i*3;
+                //gfloat *no = normals_o + i*3;
+
+                gfloat a = scale*RAD_PER_DEG*pi[1];
+                gfloat s = sin(a);
+                gfloat c = cos(a);
+
+                matrix44_identity(m);
+                matrix44_rotate_from_axis(m, a, axis[0], axis[1], axis[2]);
+
+                point3_transform(po, m, pi);
+            }
+
+            /*
+            gint i;
+            #pragma omp parallel for private(i)
+            for(i = 0; i < size_i; i++)
+            {
+                gfloat *pi = points_i + i*3;
+                gfloat *ni = normals_i + i*3;
+                gfloat *po = points_o + i*3;
+                //gfloat *no = normals_o + i*3;
+
+                po[0] = pi[0] + ni[0]*scale;
+                po[1] = pi[1] + ni[1]*scale;
+                po[2] = pi[2] + ni[2]*scale;
+            }
+            */
+        }
+    }
+
+    MotoParam *param = moto_node_get_param((MotoNode *)self, "out_pc");
+    g_value_set_object(moto_param_get_value(param), priv->pc);
+    moto_param_update_dests(param);
+}
---
+++ src/libmoto/moto-twist-node.h
@@ -0,0 +1,58 @@
+/* ##################################################################################
+#
+#  Moto Animation System (http://motoanim.sf.net)
+#  Copyleft (C) 2008 Konstantin Evdokimenko a.k.a Qew[erty] (qewerty@gmail.com)
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+################################################################################## */
+
+#ifndef __MOTO_DISPLACE_NODE_H__
+#define __MOTO_DISPLACE_NODE_H__
+
+#include "moto-geometry-view-node.h"
+
+G_BEGIN_DECLS
+
+typedef struct _MotoDisplaceNode MotoDisplaceNode;
+typedef struct _MotoDisplaceNodeClass MotoDisplaceNodeClass;
+
+/* class MotoDisplaceNode */
+
+struct _MotoDisplaceNode
+{
+    MotoNode parent;
+};
+
+struct _MotoDisplaceNodeClass
+{
+    MotoNodeClass parent;
+};
+
+GType moto_displace_node_get_type(void);
+
+#define MOTO_TYPE_DISPLACE_NODE (moto_displace_node_get_type())
+#define MOTO_DISPLACE_NODE(obj)  (G_TYPE_CHECK_INSTANCE_CAST ((obj), MOTO_TYPE_DISPLACE_NODE, MotoDisplaceNode))
+#define MOTO_DISPLACE_NODE_CLASS(klass)  (G_TYPE_CHECK_CLASS_CAST ((klass), MOTO_TYPE_DISPLACE_NODE, MotoDisplaceNodeClass))
+#define MOTO_IS_DISPLACE_NODE(obj)  (G_TYPE_CHECK_INSTANCE_TYPE ((obj),MOTO_TYPE_DISPLACE_NODE))
+#define MOTO_IS_DISPLACE_NODE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),MOTO_TYPE_DISPLACE_NODE))
+#define MOTO_DISPLACE_NODE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),MOTO_TYPE_DISPLACE_NODE, MotoDisplaceNodeClass))
+
+MotoDisplaceNode *moto_displace_node_new(const gchar *name);
+
+G_END_DECLS
+
+#endif /* __MOTO_DISPLACE_NODE_H__ */
+
