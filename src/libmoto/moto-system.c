bf28ba9b7d6d00117e1a48333d73a4e7cad31e98
Konstantin A. Evdokimenko a.k.a Qew[erty] <qewerty@gmail.com>
2008-12-02 17:20:09 +0600

    Added support of MotoBendNode to MotoSystem and testing creating of bend node with cube in MotoShelf

--- src/libmoto/moto-system.c
+++ src/libmoto/moto-system.c
@@ -10,6 +10,7 @@
 /* nodes */
 #include "moto-displace-node.h"
 #include "moto-twist-node.h"
+#include "moto-bend-node.h"
 #include "moto-object-node.h"
 #include "moto-camera-node.h"
 #include "moto-sler-material-node.h"
@@ -141,6 +142,7 @@ moto_system_class_init(MotoSystemClass *klass)
             MOTO_TYPE_SLER_MATERIAL_NODE;
         MOTO_TYPE_DISPLACE_NODE;
         MOTO_TYPE_TWIST_NODE;
+        MOTO_TYPE_BEND_NODE;
 }
 
 G_DEFINE_TYPE(MotoSystem, moto_system, G_TYPE_OBJECT);
