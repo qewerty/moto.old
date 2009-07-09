152974a0d8a86a4f3e013e9bd171f41147585845
Konstantin A. Evdokimenko a.k.a Qew[erty] <qewerty@gmail.com>
2009-07-09 14:46:20 +0700

    Added compiler arguments -Wno-unused-variable and -Wno-unused-parameter

--- config/linux2-sse.py
+++ config/linux2-sse.py
@@ -5,7 +5,7 @@ CXX = 'g++'
 
 CFLAGS  = ['-O3', '-pipe', '-ffast-math', '-Wall',
            '-march=i686', '-msse', '-msse2', '-mfpmath=sse',
-           '-fopenmp']
+           '-fopenmp', '-Wno-unused-variable', '-Wno-unused-parameter']
 LIBS    = ['gomp', 'GL', 'GLU', 'GLEW']
 
 CPPFLAGS = ['-DMOTO_WITH_WOBJ_MESH_LOADER',
@@ -14,7 +14,7 @@ CPPFLAGS = ['-DMOTO_WITH_WOBJ_MESH_LOADER',
             ]
 
 PKG_CONFIG = ['pkg-config gtk+-2.0 libglade-2.0 gtkglext-1.0 gio-2.0 gthread-2.0 --cflags --libs',
-              'python2.5-config --cflags', 'python2.5-config --libs']
+              'python2.5-config --includes', 'python2.5-config --libs']
 
 PLUGINS = []
 
--- config/linux2.py
+++ config/linux2.py
@@ -4,7 +4,7 @@ CC  = 'gcc'
 CXX = 'g++'
 
 CFLAGS  = ['-O3', '-pipe', '-ffast-math', '-Wall', '-march=i686',
-           '-fopenmp']
+           '-fopenmp','-Wno-unused-variable', '-Wno-unused-parameter']
 LIBS    = ['gomp', 'GL', 'GLU', 'GLEW']
 
 CPPFLAGS = ['-DMOTO_WITH_WOBJ_MESH_LOADER',
@@ -13,7 +13,7 @@ CPPFLAGS = ['-DMOTO_WITH_WOBJ_MESH_LOADER',
             ]
 
 PKG_CONFIG = ['pkg-config gtk+-2.0 libglade-2.0 gtkglext-1.0 gio-2.0 gthread-2.0 --cflags --libs',
-              'python2.5-config --cflags', 'python2.5-config --libs']
+              'python2.5-config --includes', 'python2.5-config --libs']
 
 PLUGINS = []
 
